2ea81f8bf1876c90db3072449cacce9c2764d94c
cheribim01 <129387028+cheribim01@users.noreply.github.com>
2023-04-27 09:58:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,29 +18,25 @@ button_a = Button(12)
 button_b = Button(13)
 button_y = Button(15)
 
-
 import time
-#from time import localtime
-
-
 import utime
 from utime import localtime
 dateTimeObj = localtime()
 
-
-    
+#this is setting up the time in DD/MM/YY format
 def time_1():
     Dyear, Dmonth, Dday, Dhour, Dmin, Dsec, Dweekday, Dyearday = (dateTimeObj)
     Ddateandtime = "{}/{}/{}"
     timex = Ddateandtime.format(Dday, Dmonth, Dyear)
     return timex
-
+#this is setting up the time in HH:MM format
 def time_2():
     Dyear, Dmonth, Dday, Dhour, Dmin, Dsec, Dweekday, Dyearday = (dateTimeObj)
     Ddateandtime_2 = "{00}:{00}"
     timey = Ddateandtime_2.format(Dhour, Dmin)
     return timey
 
+#creating the pen colours, in RGB code.
 TEMPCOLOUR = display.create_pen(0, 0, 0)  # this colour will get changed in a bit
 WHITE = display.create_pen(255, 255, 255)
 BLACK = display.create_pen(0, 0, 0)
@@ -51,8 +47,10 @@ GREEN = display.create_pen(0, 255, 0)
 LIGHTBLUE = display.create_pen(137, 207, 240)
 YELLOW = display.create_pen(255, 255, 0)
 
+#reading the sensor
 temperature, pressure, humidity = bme.read()
 
+#setting up the descriptions to be used on the display
 def describe_temperature(temperature):
     global TEMPCOLOUR
     if temperature < 10:
@@ -136,12 +134,8 @@ while True:
     display.set_pen(RED)
     display.text(describe_humidity(humidity), 10, 220, 240, 3)
 
-    rand = random.randint(1, 1)
-    
-            
     temperature, pressure, humidity = bme.read()
-            
-            
+    
     topic_msg_1 = str('temperature:' + ' ' + '{:.1f}'.format(temperature) + ' ' + 'C')
     topic_msg_2 = str('humidity:' + ' ' + '{:.0f}'.format(humidity) + ' ' + '%')
     topic_msg_3 = str('pressure:' + ' ' + '{:.0f}'.format(revised_pressure) + ' ' + 'hPa')
@@ -172,7 +166,7 @@ while True:
         display.set_pen(BLACK)
         display.clear()
         display.set_pen(WHITE)
-        display.text('button a test', 10, 10, 240, 4)
+        display.text('button a test', 10, 10, 240, 4) #feel free to ad other words here! 
         display.update()
         time.sleep(5)
         display.set_pen(BLACK)
